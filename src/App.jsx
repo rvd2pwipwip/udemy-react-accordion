46c51dc7e1d0459dc3f1c61c96556ce1bce5423f
Herve Desrosiers <hdesrosiers@stingray.com>
2026-03-25 07:41:53 -0400

    initial commit starter files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+};
+
+const Accordion = () => {
+  return <div>TODO</div>;
+};
+
+export default App;
