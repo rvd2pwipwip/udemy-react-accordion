954d9fd6e29ca6216735ef0335fbd1d1d6a2b6bc
Herve Desrosiers <hdesrosiers@stingray.com>
2026-03-25 08:37:44 -0400

    independent accordion items

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./styles.css";
 
 const faqs = [
@@ -18,13 +19,41 @@ const faqs = [
 const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 };
 
-const Accordion = () => {
-  return <div>TODO</div>;
+const Accordion = ({ data }) => {
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem
+          num={i}
+          title={item.title}
+          text={item.text}
+          key={item.title}
+        />
+      ))}
+    </div>
+  );
+};
+
+const AccordionItem = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prevIsOpen) => (prevIsOpen = !prevIsOpen));
+  };
+
+  return (
+    <div className={`item ${isOpen && "open"}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 };
 
 export default App;
